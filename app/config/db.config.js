75d5e666a89c4eab9bdb1ea20e23e1c366d47691
Manfredi <manfrediorlando@yahoo.it>
2022-09-07 13:31:37 +0200

    add config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "andrea",
-    PASSWORD: "andrea",
-    DB: "optionsfy",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "Agarthi67Devel42Hope5",
+  DB: "optionsfy",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
